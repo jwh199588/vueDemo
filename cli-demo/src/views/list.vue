ac01187d897c7e2c10803b00d9d04fa69d5d6fd6
jwh <1262466460@qq.com>
2019-04-18 14:58:43 +0800

    Create list.vue

---
+++ cli-demo/src/views/list.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item,index) in list"
+          :key="index">
+        {{item.title}}----{{item.content}}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import store from '@/store'
+export default {
+  store,
+  data () {
+    return {
+    }
+  },
+  computed: {
+    list () {
+      return store.state.news
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
