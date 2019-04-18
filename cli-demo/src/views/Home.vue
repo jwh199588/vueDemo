20854e83f42d623de1c9e33db7a8ecdcfb5796a8
jwh <1262466460@qq.com>
2019-04-18 14:58:45 +0800

    Create Home.vue

---
+++ cli-demo/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <router-view></router-view>
+    </div>
+    <div>
+      <router-link to="/home/list">新闻列表</router-link>
+      <router-link to="/home/user">个人中心</router-link>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  props: [],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
