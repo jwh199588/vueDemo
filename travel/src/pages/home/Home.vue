c6866750e94ec0db52dba3b4c7aac0fb395d76e4
jwh <1262466460@qq.com>
2019-04-18 17:29:47 +0800

    Create Home.vue
    去哪网demo

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    home
+    <router-link to='/list'>列表</router-link>
+  </div>
+</template>
+<script>
+export default {
+  props: [],
+  data () {
+    return {
+    }
+  },
+  components: {},
+  computed: {},
+  mounted: {},
+  methods: {}
+}
+</script>
+<style scoped>
+</style>
