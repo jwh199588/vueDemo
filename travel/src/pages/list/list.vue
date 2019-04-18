7828d4f62bd8cf5d596062ea68fb493eea520470
jwh <1262466460@qq.com>
2019-04-18 17:29:50 +0800

    Create list.vue
    去哪网demo

---
+++ travel/src/pages/list/list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    list
+  </div>
+</template>
+<script>
+export default {
+  props: [],
+  data () {
+    return {
+    }
+  },
+  components: {},
+  computed: {},
+  mounted: {},
+  methods: {}
+}
+</script>
+<style scoped>
+</style>
