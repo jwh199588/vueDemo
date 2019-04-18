b1ad9a5ed623dceb96f97b2f0d961914517e799e
jwh <1262466460@qq.com>
2019-04-18 14:58:40 +0800

    Create add.vue

---
+++ cli-demo/src/views/add.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text"
+           v-model="title">
+    <input type="text"
+           v-model="content">
+    <button @click="add">添加</button>
+  </div>
+</template>
+<script>
+import store from '@/store'
+export default {
+  store,
+  data () {
+    return {
+      title: '',
+      content: ''
+    }
+  },
+  methods: {
+    add () {
+      store.commit('addItem', {
+        title: this.title,
+        content: this.content
+      })
+      this.$router.push('/home/list')
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
