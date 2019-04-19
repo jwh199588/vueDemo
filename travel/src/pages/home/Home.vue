6674e7067a73adae262067246effe180e520685f
jwh <1262466460@qq.com>
2019-04-19 21:19:50 +0800

    Update Home.vue

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -1,11 +1,15 @@
+<!--首页-->
 <template>
   <div>
     <home-Header></home-Header>
-
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/swiper'
+import HomeIcons from './components/icons'
 export default {
   props: [],
   data () {
@@ -13,7 +17,9 @@ export default {
     }
   },
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
