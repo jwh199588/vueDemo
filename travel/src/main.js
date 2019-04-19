70f1905a1de65c7bdb970536b1d4925b08d0f12a
jwh <1262466460@qq.com>
2019-04-19 21:19:33 +0800

    Update main.js

--- travel/src/main.js
+++ travel/src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './assets/style/border.css'
-import './assets/style/reset.css'
+import 'style/css/border.css'
+import 'style/css/reset.css'
+import 'style/css/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import fastClick from 'fastclick'
+
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 fastClick.attach(document.body)
 new Vue({
   router,
