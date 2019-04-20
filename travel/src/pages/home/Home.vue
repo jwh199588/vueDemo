212147a280cfbe11b080f9d50e17f1fc1467756e
jwh <1262466460@qq.com>
2019-04-20 22:21:30 +0800

    Update Home.vue

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -2,24 +2,52 @@
 <template>
   <div>
     <home-Header></home-Header>
-    <home-swiper></home-swiper>
-    <home-icons></home-icons>
+    <home-swiper :swiperList="swiperList"></home-swiper>
+    <home-icons :iconList="iconList"></home-icons>
+    <home-guessList :recommendList="recommendList"></home-guessList>
+    <home-weekend :weekendList="weekendList"></home-weekend>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/swiper'
 import HomeIcons from './components/icons'
+import HomeGuessList from './components/GuessLike'
+import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
   props: [],
   data () {
     return {
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
     }
   },
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeGuessList,
+    HomeWeekend
+  },
+  mounted () {
+    this.getInfo()
+  },
+  methods: {
+    getInfo () {
+      axios.get('/api/seller').then(this.setInfo)
+    },
+    setInfo (datas) {
+      var data = datas.data.data
+      if (data.ret && data.data) {
+        this.swiperList = data.data.swiperList
+        this.iconList = data.data.iconList
+        this.recommendList = data.data.recommendList
+        this.weekendList = data.data.weekendList
+      }
+    }
   }
 }
 </script>
