938c6f23ed5cdefb67dce9f50d8c8fc092d453d8
jwh <1262466460@qq.com>
2019-04-20 22:21:44 +0800

    Create Detail.vue

---
+++ travel/src/pages/detail/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <detail-banner></detail-banner>
+  </div>
+</template>
+<script>
+import DetailBanner from './components/banner'
+export default {
+  components: {
+    DetailBanner
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
