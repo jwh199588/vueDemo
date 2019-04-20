670d58642f52db034e2cbd62e933fa26c56a4978
jwh <1262466460@qq.com>
2019-04-20 22:21:33 +0800

    Update vue.config.js

--- travel/vue.config.js
+++ travel/vue.config.js
@@ -1,4 +1,11 @@
 const path = require('path')
+/* 配置本地json */
+const express = require('express')
+const app = express()
+var appData = require('./src/assets/mock/index.json')
+var seller = appData
+var apiRoutes = express.Router()
+app.use('/api', apiRoutes)
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -11,5 +18,23 @@ module.exports = {
     config.resolve.alias
       .set('@', resolve('src'))
       .set('style', resolve('src/assets/style'))
+      .set('common', resolve('src/common'))
+  },
+  // assetsDir: 'assets', // 设置静态资源路径
+  devServer: {
+    before (app) {
+      app.get('/api/seller', (req, res) => {
+        res.json({
+          errno: 0, // 这里是你的json内容
+          data: seller
+        })
+      })
+    },
+    host: 'localhost',
+    port: 9999, // 端口号
+    https: false, // https:{type:Boolean}
+    open: true, // 配置自动启动浏览器  http://172.16.1.12:7071/rest/mcdPhoneBar/
+    hotOnly: true, // 热更新
+    proxy: null
   }
 }
