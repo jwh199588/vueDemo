139b065368fb3782d536042cd5567d06f1479d68
jwh <1262466460@qq.com>
2019-04-22 09:56:24 +0800

    Update store.js

--- travel/src/store.js
+++ travel/src/store.js
@@ -2,13 +2,23 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+let defaultCity = '北京'
+try {
+  if (localStorage.city) {
+    defaultCity = localStorage.city
+  }
+} catch (error) {}
 export default new Vuex.Store({
   state: {
-
+    city: defaultCity
   },
   mutations: {
-
+    showCity (state, city) {
+      state.city = city
+      try {
+        localStorage.city = city
+      } catch (error) {}
+    }
   },
   actions: {
 
